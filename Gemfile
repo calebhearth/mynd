61a4642764a5c9bcfd1c1ba68b0e6b5a9d095d92
Caleb Thompson <cjaysson@gmail.com>
2012-10-25 19:55:18 -0400

    This is getting scary and I'm getting tired, but here it is

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
 
 gem 'redis'
+gem 's3'
 gem 'sinatra'
--- mynd.rb
+++ mynd.rb
@@ -1,9 +1,30 @@
+require 'imagemagic'
+require 'redis'
+require 's3'
 require 'sinatra'
+
+$redis = Redis.new
+s3 = S3::Service.new(
+  access_key_id: ENV['AMAZON_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY'],
+)
+
+if $redis.get(:next_id)
+  $redis.set(:next_id, 1)
+end
 
 get '/' do
   'Nothing to see here. Move along.'
 end
 
+post '/' do
+  image = Image::Magic.from_blob(params['image'])
+  next_id = $redis.get(:next_id)
+  $redis.incr(:next_id)
+  $redis.set(next_id, image.to_blob)
+end
+
 get %r{/(\d+)} do |image_id|
-  image_id
+  $redis.get(image_id)
+  s3
 end
